3fe20cca0a02fb89ac51b422fc4d8456a8e5b49a
Christopher J Rothove <72661216+ChrisRothove@users.noreply.github.com>
2020-12-16 22:40:37 -0600

    updating

--- public/src/accounts.js
+++ public/src/accounts.js
@@ -1,10 +1,26 @@
-function findAccountById(accounts, id) {}
+function findAccountById(accounts, id) {
+  return accounts.find((account) => account.id === id);
+}
 
-function sortAccountsByLastName(accounts) {}
+function sortAccountsByLastName(accounts) {
+  return accounts.sort((accountA, accountB) => 
+  accountA.name.last.toLowerCase() > accountB.name.last.toLowerCase() ? 1 : -1);
+}
 
-function numberOfBorrows(account, books) {}
+function numberOfBorrows({id}, books) {
+  let booksBorrowed = 0;
+  books.forEach((book) => 
+    book.borrows.forEach((entry) => {
+      if (entry.id === id) booksBorrowed += 1;
+    } 
+    )
+  )
+  return booksBorrowed;
+}
 
-function getBooksPossessedByAccount(account, books, authors) {}
+function getBooksPossessedByAccount(account, books, authors) {
+  
+}
 
 module.exports = {
   findAccountById,
